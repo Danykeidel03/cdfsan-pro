487054ade37c4cec2a9ab7c234cea4b689611fad
danielfernandez <danielfernandez@craftelier.com>
2024-12-04 11:54:57 +0100

    cambios en visualizacion y smtp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
-    <link rel="shortcut icon" href="{{ asset('public/images/favicon_lienzo.png') }}">
+    <link rel="shortcut icon" href="{{ asset('images/favicon_lienzo.png') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
